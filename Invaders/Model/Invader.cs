fdcf3e41e91db014569a3e8dd8531b445056364a
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2021-06-14 19:26:39 +0300

    Refactor invader score setter

--- Invaders/Model/Invader.cs
+++ Invaders/Model/Invader.cs
@@ -8,12 +8,37 @@ namespace Invaders.Model
         public static Size InvaderSize => new(15, 15);
         public static Size MothershipSize => new(InvaderSize.Width * 5, InvaderSize.Height * 2);
         public InvaderType Type { get; }
-        public int Score { get; }
-        
+        public int Score { get; private set; }
+
         public Invader(InvaderType type, Point location, Size size) : base(location, size)
         {
             Type = type;
-            switch (Type)
+            SetScoreValue(type);
+            Speed = InvaderSpeed;
+        }
+
+        public override void Move(Direction direction)
+        {
+            Point oldLocation = Location;
+            switch (direction)
+            {
+                case Direction.Left:
+                    Location = new Point(oldLocation.X - InvaderSize.Width / 2, oldLocation.Y);
+                    break;
+                case Direction.Right:
+                    Location = new Point(oldLocation.X + InvaderSize.Width / 2, oldLocation.Y);
+                    break;
+                case Direction.Down:
+                    Location = new Point(oldLocation.X, oldLocation.Y + InvaderSize.Height);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), $"Unexpected direction '{direction}' for invader type '{Type}'.");
+            }
+        }
+
+        private void SetScoreValue(InvaderType type)
+        {
+            switch (type)
             {
                 case InvaderType.Star:
                     Score = 10;
@@ -38,25 +63,6 @@ namespace Invaders.Model
                     break;
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), $"Unexpected invader type: {type}.");
-            }
-        }
-
-        public override void Move(Direction direction)
-        {
-            Point oldLocation = Location;
-            switch (direction)
-            {
-                case Direction.Left:
-                    Location = new Point(oldLocation.X - InvaderSize.Width / 2, oldLocation.Y);
-                    break;
-                case Direction.Right:
-                    Location = new Point(oldLocation.X + InvaderSize.Width / 2, oldLocation.Y);
-                    break;
-                case Direction.Down:
-                    Location = new Point(oldLocation.X, oldLocation.Y + InvaderSize.Height);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(direction), $"Unexpected direction '{direction}' for invader type '{Type}'.");
             }
         }
     }
